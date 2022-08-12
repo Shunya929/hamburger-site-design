a31e842dd7566cc7cc5aa000593fa5319167c55d
Shunya <shunyagoto192@gmail.com>
2022-08-12 15:16:38 +0900

    hamburger

---
+++ js/main.js
@@ -0,0 +1,19 @@
+jQuery(function($){
+
+  let h_sidebar = $(window).height(); 
+  $('.js-sidebar').css('height',h_sidebar +'px');
+
+  $(".js-hamburger").on("click",function() {
+    $(this).toggleClass("is-open");
+    $(".js-sidebar").toggleClass("is-open");
+    $(".js-menubar__sidebar").toggleClass("is-open");
+    $("js-hamburger").toggleClass("is-open");
+    $(".js-background--sidebar").toggleClass("is-open");
+    if ($("h2.js-txt--changeable").text() === 'Menu') {
+      $("h2.js-txt--changeable").text('×');
+    } else {
+      $("h2.js-txt--changeable").text('Menu');
+    }
+  });
+
+});
