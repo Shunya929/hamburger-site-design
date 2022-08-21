f9ee6b98d1ae56fcbce93e0db3b29ec6ffda80db
Shunya <shunyagoto192@gmail.com>
2022-08-22 00:09:07 +0900

    hamburger

--- js/main.js
+++ js/main.js
@@ -11,9 +11,32 @@ jQuery(function($){
       .done(function() {
         $(".js-sidebar").toggleClass("is-open");
         $(".js-menubar__sidebar").toggleClass("is-open");
-        $("js-hamburger").toggleClass("is-open");
+        // $(".js-hamburger").toggleClass("is-open");
         $(".js-body").toggleClass("is-open");
-         $("h2.js-txt--changeable").text() === 'Menu' ? $("h2.js-txt--changeable").text('×'):$("h2.js-txt--changeable").text('Menu');
+        $("h2.js-txt--changeable").text() === 'Menu' ? $("h2.js-txt--changeable").text('×'):$("h2.js-txt--changeable").text('Menu');
       });
     });
+
+  $(window).resize(function() {
+    $(this).removeClass("is-open"),
+    $(".js-sidebar").removeClass("is-open");
+    $(".js-menubar__sidebar").removeClass("is-open");
+    $(".js-body").removeClass("is-open");
+    $(".js-background--sidebar").removeClass("is-open")
+    // $(".js-hamburger").removeClass("is-open");
+
+    // if($('.js-hamburger').hasClass('is-open')) {
+    //   $(".p-header-right").addClass("is-open");
+    //   $(this).addClass("is-open");
+    //   $('h2.c-menu-list').css('display','block');
+    //   $('.js-menu').stop().slideDown(300);
+    // } else {
+    //   $(this).removeClass("is-open");
+    //   $(".p-header-right").removeClass("is-open");
+    //   $('.js-menu').css('display','none');
+    //   $('.js-menu').stop().slideUp(300);
+    // }
+
+    // $("h2.js-txt--changeable").text() === '×' ? $("h2.js-txt--changeable").text('Menu'):$("h2.js-txt--changeable").text('×');
+  });
 });
